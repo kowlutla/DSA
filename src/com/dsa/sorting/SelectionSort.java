02b998a3df5c866cbed500e147d206e5dd75c997
Kowluta <kowlutla1119@gmail.com>
2023-12-28 17:21:20 +0530

    Selection sort

--- src/com/dsa/sorting/SelectionSort.java
+++ src/com/dsa/sorting/SelectionSort.java
@@ -4,7 +4,6 @@
 package com.dsa.sorting;
 
 import java.util.Arrays;
-import java.util.Random;
 
 /**
  * @author KowlutlaSwamy
@@ -18,7 +17,7 @@ public class SelectionSort implements SortingAlgorithm {
         SelectionSort selectionSort = new SelectionSort();
 
         // Generating and sorting array in increasing order
-        int[] arr1 = getIntegerArray(); // Generating an array of random integers
+        int[] arr1 = SortingAlgorithm.getIntegerArray(); // Generating an array of random integers
         System.out.println("\nBefore Sorting: \n" + Arrays.toString(arr1)); // Displaying the array before sorting
         selectionSort.sortIncreasingOrder(arr1); // Sorting the array in increasing order
         System.out.println("\nAfter Sorting: \n" + Arrays.toString(arr1)); // Displaying the array after sorting
@@ -26,26 +25,10 @@ public class SelectionSort implements SortingAlgorithm {
         System.out.println("\n===============================================\n");
 
         // Generating and sorting array in decreasing order
-        int[] arr2 = getIntegerArray(); // Generating another array of random integers
+        int[] arr2 = SortingAlgorithm.getIntegerArray(); // Generating another array of random integers
         System.out.println("\nBefore Sorting: \n" + Arrays.toString(arr2)); // Displaying the array before sorting
         selectionSort.sortDecresingOrder(arr2); // Sorting the array in decreasing order
         System.out.println("\nAfter Sorting: \n" + Arrays.toString(arr2)); // Displaying the array after sorting
-    }
-
-    /**
-     * Generate an array of random integers with random size between 10 and 19
-     */
-    private static int[] getIntegerArray() {
-        Random random = new Random(); // Creating an instance of Random class
-        int size = random.nextInt(10) + 10; // Generating a random size between 10 and 19
-        System.out.println("Size of array is: " + size); // Displaying the size of the array
-        int[] arr = new int[size]; // Creating an array with the generated size
-
-        // Filling the array with random integers between 0 and 99
-        for (int i = 0; i < size; i++) {
-            arr[i] = random.nextInt(100); // Assigning a random integer between 0 and 99 to each index
-        }
-        return arr; // Returning the generated array
     }
 
     // Sorting the array in increasing order using selection sort algorithm
--- src/com/dsa/sorting/SortingAlgorithm.java
+++ src/com/dsa/sorting/SortingAlgorithm.java
@@ -3,6 +3,8 @@
  */
 package com.dsa.sorting;
 
+import java.util.Random;
+
 /**
  * @author KowlutlaSwamy
  *
@@ -12,4 +14,17 @@ public interface SortingAlgorithm {
 	void sortIncreasingOrder(int[] arr);
 
 	void sortDecresingOrder(int[] arr);
+	
+	static int[] getIntegerArray() {
+        Random random = new Random(); // Creating an instance of Random class
+        int size = random.nextInt(10) + 10; // Generating a random size between 10 and 19
+        System.out.println("Size of array is: " + size); // Displaying the size of the array
+        int[] arr = new int[size]; // Creating an array with the generated size
+
+        // Filling the array with random integers between 0 and 99
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(100); // Assigning a random integer between 0 and 99 to each index
+        }
+        return arr; // Returning the generated array
+    }
 }
