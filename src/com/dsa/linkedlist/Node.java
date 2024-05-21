d12bc2ded59e568af5fb482c24f67861a62c515b
kowlutla <kowlutla1119.com>
2024-05-21 23:02:29 +0530

    Node class changes

--- src/com/dsa/linkedlist/Node.java
+++ src/com/dsa/linkedlist/Node.java
@@ -4,12 +4,13 @@ public class Node {
 
 	int data;
 	Node next;
-	Node prev; //to use for doubly linkedlist
+	Node prev; // to use for doubly linkedlist
 
-	Node() {
+	public Node() {
 		data = 0;
 	}
-	Node(int d) {
+
+	public Node(int d) {
 		data = d;
 	}
 }
