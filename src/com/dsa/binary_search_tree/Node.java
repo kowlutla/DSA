2e23abc84e83c8b25c0162c4e96a8c4af4e0fc54
kowlutla <kowlutla1119@gmail.com>
2024-06-16 15:00:43 +0530

    BST Node class

---
+++ src/com/dsa/binary_search_tree/Node.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.dsa.binary_search_tree;
+
+/**
+ * @author KowlutlaSwamy
+ *
+ */
+public class Node {
+
+	int data;
+	Node left;
+	Node right;
+
+	Node(int data) {
+		this.data = data;
+		left = null;
+		right = null;
+	}
+}
