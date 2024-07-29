8e62865ba8de232d5efe80e6f55928a9855955df
kowlutla <kowlutla1119@gmail.com>
2024-07-29 21:09:47 +0530

    Implement Stack using Queues(Push as costly)

---
+++ src/com/dsa/stack/ST3StackUsingQueue1.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package com.dsa.stack;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 
+ * Implementing stack using queue by making push operation as costly
+ * 
+ */
+public class ST3StackUsingQueue1<T> implements Stack<T> {
+	
+	private Queue<T> original;
+	private Queue<T> auxiliary;
+	
+	public ST3StackUsingQueue1() {
+		
+		original = new LinkedList<T>();
+		auxiliary = new LinkedList<T>();
+		
+	}
+	
+	@Override
+	public void push(T data) {
+		
+		// Move all elements of original queue to other queue
+		while (!original.isEmpty()) {
+			auxiliary.add(original.remove());
+		}
+		
+		// add the current element at the first of original queue
+		original.add(data);
+		
+		// move all elements of aux queue to original queue
+		while (!auxiliary.isEmpty()) {
+			original.add(auxiliary.remove());
+		}
+		
+	}
+	
+	@Override
+	public T pop() {
+		
+		if (isEmpty()) {
+			throw new RuntimeException("Stack is Empty");
+		}
+		return original.remove();
+		
+	}
+	
+	@Override
+	public T peek() {
+		
+		if (isEmpty()) {
+			throw new RuntimeException("Stack is Empty");
+		}
+		return original.peek();
+		
+	}
+	
+	@Override
+	public int size() {
+		
+		return original.size();
+		
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		
+		return original.isEmpty();
+		
+	}
+	
+	@Override
+	public boolean isFull() {
+		
+		return false;
+		
+	}
+	
+	public String toString() {
+		
+		return String.valueOf(original);
+		
+	}
+}
--- src/com/dsa/stack/StackDemo.java
+++ src/com/dsa/stack/StackDemo.java
@@ -11,7 +11,7 @@ public class StackDemo {
 	
 	public static void main(String[] args) {
 		
-		Stack<Integer> stack = new ST2LinkedStack<Integer>();
+		Stack<Integer> stack = new ST3StackUsingQueue1<Integer>();
 		stack.push(10);
 		System.out.println(stack);
 		stack.push(11);
