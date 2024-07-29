572e9ad1383f6eba30408644780fe0d0fcee4504
kowlutla <kowlutla1119@gmail.com>
2024-07-29 19:35:49 +0530

    Queue implementation using Singly LinkedList

---
+++ src/com/dsa/queue/LinkedQueue.java
@@ -0,0 +1,136 @@
+/**
+ * 
+ */
+package com.dsa.queue;
+
+/**
+ * @author KowlutlaSwamy
+ *
+ */
+public class LinkedQueue<T> implements Queue<T> {
+	
+	private static class Node<T> {
+		private T data;
+		private Node<T> next;
+		
+		public Node(T data) {
+			
+			this.data = data;
+			this.next = null;
+			
+		}
+		
+		public T getData() {
+			
+			return data;
+			
+		}
+		
+		public void setData(T data) {
+			
+			this.data = data;
+			
+		}
+		
+		public Node<T> getNext() {
+			
+			return next;
+			
+		}
+		
+		public void setNext(Node<T> next) {
+			
+			this.next = next;
+			
+		}
+		
+	}
+	
+	private int size;
+	private Node<T> head;
+	private Node<T> tail;
+	
+	public LinkedQueue() {
+		
+		this.head = null;
+		this.tail = null;
+		this.size = 0;
+		
+	}
+	
+	@Override
+	public void add(T data) {
+		
+		Node<T> newNode = new Node<T>(data);
+		
+		if (head == null) {
+			head = tail = newNode;
+		} else {
+			tail.setNext(newNode);
+			tail = newNode;
+		}
+		size++;
+		
+	}
+	
+	@Override
+	public T remove() {
+		
+		if (isEmpty()) {
+			throw new RuntimeException("Queue Empty");
+		}
+		T data = head.getData();
+		head = head.getNext();
+		size--;
+		return data;
+		
+	}
+	
+	@Override
+	public T peek() {
+		
+		if (isEmpty()) {
+			throw new RuntimeException("Queue Empty");
+		}
+		return head.getData();
+		
+	}
+	
+	@Override
+	public int size() {
+		
+		return size;
+		
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		
+		return size() == 0;
+		
+	}
+	
+	@Override
+	public boolean isFull() {
+		
+		return false;
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuffer sb = new StringBuffer();
+		Node<T> current = head;
+		String deli = "";
+		
+		while (current != null) {
+			sb.append(deli).append(current.getData());
+			current = current.getNext();
+			deli = "->";
+		}
+		return sb.toString();
+		
+	}
+	
+}
--- src/com/dsa/queue/QueueDemo.java
+++ src/com/dsa/queue/QueueDemo.java
@@ -11,7 +11,7 @@ public class QueueDemo {
 	
 	public static void main(String[] args) {
 		
-		Queue<Integer> q = new Q1ArrayQueue<Integer>();
+		Queue<Integer> q = new LinkedQueue<Integer>();
 		q.add(10);
 		System.out.println(q);
 		q.add(11);
@@ -35,6 +35,7 @@ public class QueueDemo {
 //		System.out.println(q.peek());
 		
 		System.out.println("Size Of Queue: " + q.size());
+		System.out.println(q);
 		System.out.println("Queue Elements: ");
 		
 		while (!q.isEmpty()) {
